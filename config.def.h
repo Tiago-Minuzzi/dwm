11a8a5d2ceafbac32a51ac163b00f3d494d34eba
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-10-28 15:37:01 -0300

    Fonts awesome added

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=10", "Font Awesome 5 Free Solid:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#171a1f";
 static const char col_gray2[]       = "#444444";
