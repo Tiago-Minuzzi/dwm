6dcdbf74a7f7aa352639764649ddc3f676a52c46
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2022-05-29 21:39:39 -0300

    Font awesome 6

--- config.def.h
+++ config.def.h
@@ -16,7 +16,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono Medium:size=10", "Font Awesome 5 Free Solid:size=10", "JoyPixels:style=Regular:size=10" };
+static const char *fonts[]          = { "JetBrains Mono Medium:size=10", "Font Awesome 6 Free Solid:size=10", "JoyPixels:style=Regular:size=10" };
 static const char dmenufont[]       = "JetBrains Mono Medium:size=10";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
