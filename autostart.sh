b2cbc782194b8d7e7447739c017c7cf26c5e9a52
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2022-06-22 08:59:23 -0300

    Warp mouse arrow patch

--- autostart.sh
+++ autostart.sh
@@ -10,7 +10,7 @@ rshift () {
 
 exec slstatus &
 nm-applet &
-xrandr --output HDMI1 --mode 1920x1080 --left-of eDP --output eDP --off &
+#xrandr --output HDMI1 --mode 1920x1080 --left-of eDP --output eDP --off &
 #xrandr --output HDMI1 --off --left-of eDP --output eDP --mode 1366x768 &
 #picom --config /home/tiago/.config/picom/picom.conf &
 dunst &
