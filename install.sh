3019e4b2fbb6bc5ae64797438ece010941f07a77
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-06-13 09:39:39 -0300

    Install xorg-xrandr, build dmenu and slstatus

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "=============================================================="
 echo "|| By running this script you're allowing SUDO privilegies! ||"
 echo "=============================================================="
 
-sudo pacman -S --needed alacritty brightnessctl feh libxft playerctl polkit-kde-agent ranger sxhkd ttf-font-awesome ttf-jetbrains-mono ttf-joypixels ueberzug unclutter xcb-util-image xorg-setxkbmap
+sudo pacman -S --needed alacritty brightnessctl feh libxft playerctl polkit-kde-agent ranger sxhkd ttf-font-awesome ttf-jetbrains-mono ttf-joypixels ueberzug unclutter xorg-setxkbmap xorg-xrandr
 
 if command -v $HELPER > /dev/null;then
     $HELPER -S --needed rofi-greenclip
@@ -16,5 +16,7 @@ else
 fi
 
 echo "### Cloning dmenu and slstatus"
-git clone --depth 1 https://github.com/Tiago-Minuzzi/dmenu $HOME/.config/dmenu
-git clone --depth 1 https://github.com/Tiago-Minuzzi/slstatus $HOME/.config/slstatus
+git clone --depth 1 https://github.com/Tiago-Minuzzi/dmenu $HOME/.config/dmenu && cd $HOME/.config/dmenu && sudo make clean install
+git clone --depth 1 https://github.com/Tiago-Minuzzi/slstatus $HOME/.config/slstatus && cd $HOME/.config/dmenu && sudo make clean install \
+cd && echo "You may need to restart dwm to run slstatus"
+
