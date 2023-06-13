3c7ef7c9b946356cadf456ec147fc1fceaef000e
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-06-13 09:13:45 -0300

    Clone dmenu and slstatus repos

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 HELPER=yay
 
-pacman -S --needed alacritty brightnessctl dmenu feh playerctl polkit-kde-agent ranger  slstatus sxhkd ttf-font-awesome ttf-jetbrains-mono ttf-joypixels ueberzug unclutter xcb-util-image xorg-setxkbmap
+pacman -S --needed alacritty brightnessctl feh playerctl polkit-kde-agent ranger sxhkd ttf-font-awesome ttf-jetbrains-mono ttf-joypixels ueberzug unclutter xcb-util-image xorg-setxkbmap
 
 if command -v $HELPER > /dev/null;then
     $HELPER -S --needed rofi-greenclip
@@ -10,3 +10,6 @@ else
     echo "${HELPER} is not installed"
     exit 1
 fi
+
+git clone --depth 1 https://github.com/Tiago-Minuzzi/dmenu $HOME/.config/dmenu
+git clone --depth 1 https://github.com/Tiago-Minuzzi/slstatus $HOME/.config/slstatus
