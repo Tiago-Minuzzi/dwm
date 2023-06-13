5d1ea8bbdf91fe210eee14356545f49fe2b8dffd
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-06-13 10:07:19 -0300

    Build dwm from install script

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ echo "=============================================================="
 echo "|| By running this script you're allowing SUDO privilegies! ||"
 echo "=============================================================="
 
-sudo pacman -S --needed alacritty brightnessctl feh libxft playerctl polkit-kde-agent ranger sxhkd ttf-font-awesome ttf-jetbrains-mono ttf-joypixels ueberzug unclutter xorg-setxkbmap xorg-xrandr
+sudo pacman -S --needed alacritty brightnessctl feh libxft playerctl polkit-kde-agent ranger sxhkd ttf-font-awesome ttf-jetbrains-mono ttf-joypixels ueberzug unclutter xorg-setxkbmap xorg-xrandr && \
+
+echo "### Building dwm ###"
+sudo make clean install && \
 
 if command -v $HELPER > /dev/null;then
     $HELPER -S --needed rofi-greenclip
@@ -15,7 +18,7 @@ else
     exit 1
 fi
 
-echo "### Cloning dmenu and slstatus"
+echo "### Cloning dmenu and slstatus ###"
 git clone --depth 1 https://github.com/Tiago-Minuzzi/dmenu $HOME/.config/dmenu && cd $HOME/.config/dmenu && sudo make clean install && \
 git clone --depth 1 https://github.com/Tiago-Minuzzi/slstatus $HOME/.config/slstatus && cd $HOME/.config/slstatus && sudo make clean install && \
 cd && echo "You may need to restart dwm to run slstatus"
