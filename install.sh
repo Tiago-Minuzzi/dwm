13cc6010f5a3bd08037c9a4300cff6526f06aea1
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-06-13 09:27:00 -0300

    Install libxft

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 HELPER=yay
 
-pacman -S --needed alacritty brightnessctl feh playerctl polkit-kde-agent ranger sxhkd ttf-font-awesome ttf-jetbrains-mono ttf-joypixels ueberzug unclutter xcb-util-image xorg-setxkbmap
+echo "=============================================================="
+echo "|| By running this script you're allowing SUDO privilegies! ||"
+echo "=============================================================="
+
+sudo pacman -S --needed alacritty brightnessctl feh libxft playerctl polkit-kde-agent ranger sxhkd ttf-font-awesome ttf-jetbrains-mono ttf-joypixels ueberzug unclutter xcb-util-image xorg-setxkbmap
 
 if command -v $HELPER > /dev/null;then
     $HELPER -S --needed rofi-greenclip
@@ -11,5 +15,6 @@ else
     exit 1
 fi
 
+echo "### Cloning dmenu and slstatus"
 git clone --depth 1 https://github.com/Tiago-Minuzzi/dmenu $HOME/.config/dmenu
 git clone --depth 1 https://github.com/Tiago-Minuzzi/slstatus $HOME/.config/slstatus
