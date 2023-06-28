632e3a8684896e99cdd78e8a485388b64edc45b3
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-06-28 19:52:06 -0300

    Font color of selected items changed in catppuccin theme

--- themes/catppuccin.h
+++ themes/catppuccin.h
@@ -5,7 +5,7 @@ static const char col_gray2[]       = "#6e738d";
 // Font color
 static const char col_gray3[]       = "#cad3f5";
 // Current tag and current window font color
-static const char col_gray4[]       = "#494d64";
+static const char col_gray4[]       = "#363a4f";
 // Top bar second color (blue) and active window border color
 static const char col_cyan[]        = "#8aadf4";
 
